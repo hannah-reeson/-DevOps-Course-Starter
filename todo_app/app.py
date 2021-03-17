59e28d57ba9cb0b64a0c64219741e66d5bc2a4de
Hannah Reeson <hanny-han@hotmail.co.uk>
2021-03-17 13:33:45 +0000

    list items rendered

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,14 +1,16 @@
-from flask import Flask
-
+from flask import Flask, render_template
 from todo_app.flask_config import Config
+from todo_app.data.session_items import *
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
-
+ 
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    items = get_items()
+    return render_template('index.html', items = items)
 
 
 if __name__ == '__main__':
