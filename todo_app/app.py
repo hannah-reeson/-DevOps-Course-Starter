6945e1c7207020dc173541795821cf7b5de69dd8
Hannah Reeson <hanny-han@hotmail.co.uk>
2021-03-17 14:29:21 +0000

    add items completed

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from todo_app.flask_config import Config
 from todo_app.data.session_items import *
 
@@ -7,11 +7,18 @@ app = Flask(__name__)
 app.config.from_object(Config)
  
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     items = get_items()
     return render_template('index.html', items = items)
 
+@app.route('/', methods=['POST'])
+def addItems():
+    add_item(request.form.get('title'))
+    return redirect("/")
+    
+
+   
 
 if __name__ == '__main__':
     app.run()
