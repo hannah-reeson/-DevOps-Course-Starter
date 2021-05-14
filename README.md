c95cbf134ee08506a5681dee8ed2bf1b761f2073
Hannah Reeson <hanny-han@hotmail.co.uk>
2021-05-14 14:28:51 +0100

    e excersize 2 complete

--- README.md
+++ README.md
@@ -50,3 +50,14 @@ You should see output similar to the following:
  * Debugger PIN: 226-556-590
 ```
 Now visit [`http://localhost:5000/`](http://localhost:5000/) in your web browser to view the app.
+
+### Trello API Information
+
+For this application to work with Trello's API, you will need to update the following in the .env file:
+
+TRELLO_KEY: Your authorization key from Trello
+TRELLO_TOKEN: Your security token from Trello
+BOARD_ID: The board ID for the board your using
+ 
+Your board must have the following lists: to-do, doing and done.
+
--- todo_app/app.py
+++ todo_app/app.py
@@ -1,24 +1,33 @@
 from flask import Flask, render_template, request, redirect
 from todo_app.flask_config import Config
-from todo_app.data.session_items import *
-
+from todo_app.data.session_items import addCard, getLists, getCards, moveCard, removeCard
 
 app = Flask(__name__)
 app.config.from_object(Config)
  
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def index():
-    items = get_items()
-    return render_template('index.html', items = items)
+    return render_template('index.html', lists=getLists(),cards=getCards())
 
-@app.route('/', methods=['POST'])
-def addItems():
-    add_item(request.form.get('title'))
-    return redirect("/")
-    
 
-   
+@app.route('/',methods=['POST'])
+def test():
+    name=request.form.get("card_name")
+    addCard(name,request.form['submit_button'])
+    return redirect('/')
+
+
+@app.route('/move/<card_id>/<list_id>',methods=['GET','POST'])
+def move(card_id,list_id):
+    moveCard(card_id,list_id)
+    return redirect('/')
+
+
+@app.route('/remove/<card_id>',methods=['GET','POST'])
+def remove(card_id):
+    removeCard(card_id)
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run()
